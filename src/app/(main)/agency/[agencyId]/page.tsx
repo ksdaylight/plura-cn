9529fdb1dfd1d56fbc64443670d9976501f2c34a
ksdaylight <yuanmiwen111@163.com>
2024-03-01 19:28:45 +0800

    feat: custom model

---
+++ src/app/(main)/agency/[agencyId]/page.tsx
@@ -0,0 +1,21 @@
+
+
+import React from 'react'
+
+const Page = async ({
+  params,
+}: {
+  params: { agencyId: string }
+}) => {
+ 
+
+
+  return (
+    <div
+    >
+      {params.agencyId}
+    </div>
+  )
+}
+
+export default Page
